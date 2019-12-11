8cfb18aec383c765e3952a466dc229542b43de5b
Joachim Doerr <mail@joachim-doerr.com>
2019-12-11 11:58:35 +0100

    add options

--- lib/Uppy/Handler/UppyUploadHandler.php
+++ lib/Uppy/Handler/UppyUploadHandler.php
@@ -9,16 +9,36 @@ namespace Uppy\Handler;
 
 use rex;
 use rex_addon;
+use rex_csrf_token;
 use rex_extension;
 use rex_extension_point;
 use rex_path;
 use rex_request;
 use rex_response;
+use Whoops\Exception\ErrorException;
 
 class UppyUploadHandler
 {
     const MEDIA_PATH = 'media/';
     const MEDIA_TYPE_PATH = '';
+
+    /**
+     * @param $filename
+     * @return mixed|string
+     * @author Joachim Doerr
+     */
+    public static function getFilename($filename)
+    {
+        $pathInfo = pathinfo($filename);
+        if (file_exists($filename)) {
+            $cnt = 1;
+            while (file_exists($pathInfo['dirname'] . '/' . $pathInfo['filename'] . '_' . $cnt . '.' . $pathInfo['extension'])) {
+                ++$cnt;
+            }
+            return $pathInfo['filename'] . '_' . $cnt . '.' . $pathInfo['extension'];
+        }
+        return $pathInfo['basename'];
+    }
 
     /**
      * @author Joachim Doerr
@@ -31,37 +51,61 @@ class UppyUploadHandler
             }
         }
 
+        // TODO load uppy profile
+        // TODO use profile path
+        // TODO use given path
 
 
-//        $basePath = "uppy";
-//        $_file = $_FILES['rex_uppy_file'];
-//        $name = $_file['name'];
-//        $file = rex_path::frontend("$basePath/$name");
-//        move_uploaded_file($_file['tmp_name'], $file);
-//
-//        $result = json_encode(array(
-//            'dirname' => pathinfo($file, PATHINFO_DIRNAME),
-//            'filename' => basename($name),
-//            'pathinfo' => pathinfo($file)
-//        ));
-//
-//        rex_response::sendContent($result, 'application/json');
-//        die;
+        try {
+            $_file = $_FILES['rex_uppy_file'];
+            $token = rex_csrf_token::factory('uppy');
 
-
-        $_file = $_FILES['rex_uppy_file'];
-
-        if ($_file['name'] != '' && rex_mediapool_isAllowedMediaType($_file['name'])) {
-
-            $userLogin = (is_object(rex::getUser())) ? rex::getUser()->getValue('login') : null;
-
-            $mediaCategory = rex_request::get('media_category', 'int', 0);
-            $return = rex_mediapool_saveMedia($_file, $mediaCategory, ['title'=>''], $userLogin);
-
-            if ($return['ok'] == 1) {
-                rex_extension::registerPoint(new rex_extension_point('MEDIA_ADDED', '', $return));
+            if (!$token->isValid()) {
+                throw new \ErrorException('csrf_token_invalid', 401);
             }
-            $file = rex_path::frontend(self::MEDIA_PATH . $return['filename']);
+            if (!isset($_file['name']) || is_null($_file)) {
+                throw new \ErrorException('internal_server_error', 500);
+            }
+
+            if (!is_null($path = rex_request::get('data_path', 'string', null))) {
+
+                // TODO replace with uppy profile it case of profile is available
+                $overwrite = rex_request::get('overwrite', 'bool', false);
+                $rename = rex_request::get('rename', 'bool', true);
+
+                $dstFile = rex_path::data($path . $_file['name']);
+                $dstPathInfo = pathinfo($dstFile);
+
+                if (!$overwrite && file_exists($dstFile) && !$rename) {
+                    throw new ErrorException('file_already_exist', 406);
+                }
+                if (!$overwrite && $rename) {
+                    $dstFile = $dstPathInfo['dirname'] . '/' . self::getFilename($dstFile);
+                }
+                if (!file_exists($dstPathInfo['dirname'])) {
+                    mkdir($dstPathInfo['dirname'], 0755, true);
+                }
+                if (!move_uploaded_file($_file['tmp_name'], $dstFile)) {
+                    throw new ErrorException('file_save_failed', 500);
+                }
+                $file = $dstFile;
+
+            } else {
+                if (rex_mediapool_isAllowedMediaType($_file['name'])) {
+
+                    $userLogin = (is_object(rex::getUser())) ? rex::getUser()->getValue('login') : null;
+                    $mediaCategory = rex_request::get('media_category', 'int', 0);
+                    $return = rex_mediapool_saveMedia($_file, $mediaCategory, ['title' => ''], $userLogin);
+
+                    if ($return['ok'] == 1) {
+                        rex_extension::registerPoint(new rex_extension_point('MEDIA_ADDED', '', $return));
+                    }
+                    $file = rex_path::frontend(self::MEDIA_PATH . $return['filename']);
+
+                } else {
+                    throw new \ErrorException('internal_server_error', 500);
+                }
+            }
 
             $statusCode = 201;
             $response = [
@@ -70,21 +114,37 @@ class UppyUploadHandler
                 'pathinfo' => pathinfo($file),
                 'uploaded' => 1,
                 'error' => null,
-                'url' => $file
+                'url' => $file,
             ];
 
-        } else {
+        } catch (\ErrorException $e) {
+            \rex_logger::logException($e);
 
-            $statusCode = 500;
+            switch ($e->getCode()) {
+                default:
+                case 500:
+                    $statusCode = 500;
+                    $errorMessage = 'Internal server error. The uploaded file was failed';
+                    break;
+                case 401:
+                    $statusCode = 401;
+                    $errorMessage = 'Csrf token is invalid. The upload was failed';
+                    break;
+                case 406:
+                    $statusCode = 406;
+                    $errorMessage = 'File already exist. The upload was failed';
+                    break;
+            }
+
             $response = [
                 'dirname' => null,
                 'filename' => null,
                 'pathinfo' => null,
                 'uploaded' => [
-                    'number'    => 500,
-                    'message'   => 'Internal server error. The uploaded file was failed',
+                    'number' => $statusCode,
+                    'message' => $errorMessage,
                 ],
-                'error' => 'internal_server_error',
+                'error' => $e->getMessage(),
                 'url' => null,
             ];
         }
@@ -93,7 +153,5 @@ class UppyUploadHandler
         rex_response::setHeader('status', $statusCode);
         rex_response::sendContent(json_encode($response), 'application/json');
         exit;
-
     }
-
 }
